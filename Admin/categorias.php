da46a306c650c110a7c171e7e30680ae5cdc70dc
Ethan John Blight <ethan@Ethans-MacBook-Pro.local>
2020-10-21 20:37:44 -0500

    actualización del proyecto

--- Admin/categorias.php
+++ Admin/categorias.php
@@ -160,31 +160,7 @@
   </div>
 </div>
 <?php
-    if(isset($_POST['anadir'])){
-        $categoria = $_POST['Nuevacategoria'];
-        $descripcionP = $_POST['nuevadescripcion'];
-        $imagen = addslashes(file_get_contents($_FILES['archivo']['tmp_name']));
-
-        $consulta = "INSERT INTO Categorias(Categoria, Descripcion, Foto) VALUES ('$categoria','$descripcionP','$imagen')";
-        $result = mysqli_query($conex, $consulta);
-        
-        if($result){
-            ?>
-            <p>Producto Guardado</p>
-            <script>
-                alert('Producto Guardad.');
-            </script>
-            <?php
-        }else{
-            ?>
-            <p>Error</p>
-            <script>
-                alert('Ocurrio un Error al registrar los datos')
-            </script>
-            <?php
-        }
-    }
-    echo'hola';
+include('vistas/regiCat.php');
 ?>
 
 
--- Admin/vistas/regiCat.php
+++ Admin/vistas/regiCat.php
@@ -5,17 +5,10 @@
         $imagen = addslashes(file_get_contents($_FILES['archivo']['tmp_name']));
 
         $consulta = "INSERT INTO Categorias(Categoria, Descripcion, Foto) VALUES ('$categoria','$descripcionP','$imagen')";
-        $result = $conex->query($sql);
-        $result->execute();
-
+        $result = mysqli_query($conex, $consulta);
         
         if($result){
-            ?>
-            <p>Producto Guardado</p>
-            <script>
-                alert('Producto Guardad.');
-            </script>
-            <?php
+            header("Location: ./Admin/Dashboard.php?op=3");
         }else{
             ?>
             <p>Error</p>
