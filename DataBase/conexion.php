2bed30acaac396a7effdbabc455b8cfcbfe989b7
Ethan John <61813114+ethanial1@users.noreply.github.com>
2020-10-22 08:53:40 -0500

    Update conexion.php
    Se añadieron los datos correctos de la base de datos.

--- DataBase/conexion.php
+++ DataBase/conexion.php
@@ -1,10 +1,10 @@
 <?php 
 class Conexion{	  
     public static function Conectar() {        
-        define('servidor', 'localhost');
-        define('nombre_bd', 'cocina');
-        define('usuario', 'root');
-        define('password', 'root');					        
+        define('servidor', 'beirmoae6wnbbif9kpqo-mysql.services.clever-cloud.com');
+        define('nombre_bd', 'beirmoae6wnbbif9kpqo');
+        define('usuario', 'u9dtlbktibcwz5cc');
+        define('password', 'BAd9l8dQ6cjoIqUTdFbK');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');	
         		
         try{
