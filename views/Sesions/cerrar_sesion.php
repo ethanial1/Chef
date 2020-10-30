4c044e6b1ff957da90ecb17473e6c0b090728219
Ethan John <61813114+ethanial1@users.noreply.github.com>
2020-10-30 09:19:46 -0600

    Update cerrar_sesion.php

--- views/Sesions/cerrar_sesion.php
+++ views/Sesions/cerrar_sesion.php
@@ -3,7 +3,7 @@ include('user_sesion.php');
 $user_sesion1 = new userSesion();
 $user_sesion1->closeSesion();
 
-header('Location: http://localhost:8888/Recetario/index.php?idx=1');
+header('Location: https://poloatlas.herokuapp.com/index.php?idx=1');
 die();
 
 ?>
